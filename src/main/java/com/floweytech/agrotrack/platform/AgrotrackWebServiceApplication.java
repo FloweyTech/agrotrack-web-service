e7d81b26ee186edc58340c88fbdb3486996f01e3
njlmrvllr <anjaliamarovillar@outlook.com>
2025-10-18 22:02:50 -0500

    initial commit

---
+++ src/main/java/com/floweytech/agrotrack/platform/AgrotrackWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.floweytech.agrotrack.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgrotrackWebServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgrotrackWebServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/floweytech/agrotrack/platform/AgrotrackWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.floweytech.agrotrack.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgrotrackWebServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
