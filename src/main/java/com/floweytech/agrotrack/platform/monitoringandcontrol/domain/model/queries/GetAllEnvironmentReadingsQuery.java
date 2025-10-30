e11b71f8543f299d37d38c4f5c34ba824af703ea
njlmrvllr <anjaliamarovillar@outlook.com>
2025-10-29 21:03:50 -0500

    feat(environment-readings): add queries for retrieving environment readings by plot ID and all readings

---
+++ src/main/java/com/floweytech/agrotrack/platform/monitoringandcontrol/domain/model/queries/GetAllEnvironmentReadingsQuery.java
@@ -0,0 +1,7 @@
+package com.floweytech.agrotrack.platform.monitoringandcontrol.domain.model.queries;
+
+/**
+ * Get all EnvironmentReadings query
+ */
+public record GetAllEnvironmentReadingsQuery() {
+}
