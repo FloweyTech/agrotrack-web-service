ebbf1991f1870de6bff34d237ac638b1cc02da23
QuiqueVladimir <89040472+QuiqueVladimir@users.noreply.github.com>
2025-10-31 09:32:20 -0500

    feat: add and update value objects for plant type domain

---
+++ src/main/java/com/floweytech/agrotrack/platform/organization/domain/model/valueobject/PlantTypes.java
@@ -0,0 +1,16 @@
+package com.floweytech.agrotrack.platform.organization.domain.model.valueobject;
+
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public enum PlantTypes {
+    POTATO,
+    CORN,
+    WHEAT,
+    BARLEY,
+    COFFEE,
+    RICE,
+    TOMATO,
+    LETTUCE,
+    CARROT
+}
