529e1dd59593a2538af5920e32ec17c66fc1f6e6
njlmrvllr <anjaliamarovillar@outlook.com>
2025-10-30 23:18:06 -0500

    feat(environment-readings): implement command and query services for environment readings

---
+++ src/main/java/com/floweytech/agrotrack/platform/monitoringandcontrol/application/internal/commandservices/EnvironmentReadingQueryServicelmpl.java
@@ -0,0 +1,43 @@
+package com.floweytech.agrotrack.platform.monitoringandcontrol.application.internal.commandservices;
+
+
+import com.floweytech.agrotrack.platform.monitoringandcontrol.domain.model.aggregates.EnvironmentReading;
+import com.floweytech.agrotrack.platform.monitoringandcontrol.domain.model.queries.GetAllEnvironmentReadingsByPlotIdQuery;
+import com.floweytech.agrotrack.platform.monitoringandcontrol.domain.model.queries.GetAllEnvironmentReadingsQuery;
+import com.floweytech.agrotrack.platform.monitoringandcontrol.domain.model.queries.GetEnvironmentReadingByIdQuery;
+import com.floweytech.agrotrack.platform.monitoringandcontrol.domain.services.EnvironmentReadingQueryService;
+import com.floweytech.agrotrack.platform.monitoringandcontrol.infrastructure.persistence.jpa.EnvironmentReadingRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+/**
+ * Environment Reading Query Service Implementation
+ */
+@Service
+public class EnvironmentReadingQueryServicelmpl implements EnvironmentReadingQueryService {
+    private final EnvironmentReadingRepository environmentReadingRepository;
+
+    /**
+     * Constructor
+     * @param environmentReadingRepository The {@link EnvironmentReadingRepository} instance
+     */
+    public EnvironmentReadingQueryServicelmpl(EnvironmentReadingRepository environmentReadingRepository){
+        this.environmentReadingRepository = environmentReadingRepository;
+    }
+
+    @Override
+    public Optional<EnvironmentReading> handle(GetEnvironmentReadingByIdQuery query) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<EnvironmentReading> handle(GetAllEnvironmentReadingsQuery query) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<EnvironmentReading> handle(GetAllEnvironmentReadingsByPlotIdQuery query) {
+        return Optional.empty();
+    }
+}
