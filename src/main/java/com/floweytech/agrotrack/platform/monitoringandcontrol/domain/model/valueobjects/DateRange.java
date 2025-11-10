575eca747506b176346034dcc31bbe6118a4fa39
njlmrvllr <anjaliamarovillar@outlook.com>
2025-11-09 23:06:13 -0500

    feat(date): refactor DateRange to use LocalDate instead of SQL Date

--- src/main/java/com/floweytech/agrotrack/platform/monitoringandcontrol/domain/model/valueobjects/DateRange.java
+++ src/main/java/com/floweytech/agrotrack/platform/monitoringandcontrol/domain/model/valueobjects/DateRange.java
@@ -3,22 +3,23 @@ package com.floweytech.agrotrack.platform.monitoringandcontrol.domain.model.valu
 import jakarta.persistence.Embeddable;
 import lombok.Getter;
 
-import java.sql.Date;
+import java.time.LocalDate;
 
 @Embeddable
 @Getter
 public class DateRange {
-    private Date startDate;
-    private Date endDate;
+    private LocalDate startDate;
+    private LocalDate endDate;
 
     protected DateRange() {
     }
-    public DateRange(Date startDate, Date endDate) {
+
+    public DateRange(LocalDate startDate, LocalDate endDate) {
         if (startDate == null)
             throw new IllegalArgumentException("startDate cannot be null");
         if (endDate == null)
             throw new IllegalArgumentException("endDate cannot be null");
-        if (endDate.before(startDate))
+        if (endDate.isBefore(startDate))
             throw new IllegalArgumentException("endDate cannot be before startDate");
         this.startDate = startDate;
         this.endDate = endDate;
