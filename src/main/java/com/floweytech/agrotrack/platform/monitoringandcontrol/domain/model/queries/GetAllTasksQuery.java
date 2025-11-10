36fb8fce7ad106d8718e82c6732a1eba020a9bfe
njlmrvllr <anjaliamarovillar@outlook.com>
2025-11-10 02:02:59 -0500

    fix(query): adjust formatting in GetAllTasksQuery record

--- src/main/java/com/floweytech/agrotrack/platform/monitoringandcontrol/domain/model/queries/GetAllTasksQuery.java
+++ src/main/java/com/floweytech/agrotrack/platform/monitoringandcontrol/domain/model/queries/GetAllTasksQuery.java
@@ -1,7 +1,8 @@
 package com.floweytech.agrotrack.platform.monitoringandcontrol.domain.model.queries;
 
+
 /**
  * Query to get all tasks.
  */
-public record GetAllTasksQuery() {
+public record GetAllTasksQuery()  {
 }
