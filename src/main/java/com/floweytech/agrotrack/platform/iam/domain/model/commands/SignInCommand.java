f3df448dad24e41e85c4bb960077fe548e612936
diegovilcasaboya <diegovilcatut@gmail.com>
2025-11-24 23:06:58 -0500

    feat(iam): add commands

--- src/main/java/com/floweytech/agrotrack/platform/iam/domain/model/commands/SignInCommand.java
+++ src/main/java/com/floweytech/agrotrack/platform/iam/domain/model/commands/SignInCommand.java
@@ -1,7 +1,14 @@
 package com.floweytech.agrotrack.platform.iam.domain.model.commands;
 
-public record SignInCommand(
-        String identifier,
-        String password
-) {
+/**
+ * Sign in command
+ * <p>
+ *     This class represents the command to sign in a user.
+ * </p>
+ * @param username the username of the user
+ * @param password the password of the user
+ *
+ * @see com.acme.center.platform.iam.domain.model.aggregates.User
+ */
+public record SignInCommand(String username, String password) {
 }
--- src/main/java/com/floweytech/agrotrack/platform/iam/domain/model/commands/SignUpCommand.java
+++ src/main/java/com/floweytech/agrotrack/platform/iam/domain/model/commands/SignUpCommand.java
@@ -1,12 +1,19 @@
 package com.floweytech.agrotrack.platform.iam.domain.model.commands;
+import com.floweytech.agrotrack.platform.iam.domain.model.entities.Role;
 import com.floweytech.agrotrack.platform.iam.domain.model.valueobjects.Roles;
 
-public record SignUpCommand(
-   String username,
-   String email,
-   String password,
-   Roles role,
-   String firstName,
-   String lastName,
-   String photoUrl
-) {}
+import java.util.List;
+
+/**
+ * Sign up command
+ * <p>
+ *     This class represents the command to sign up a user.
+ * </p>
+ * @param username the username of the user
+ * @param password the password of the user
+ * @param roles the roles of the user
+ *
+ * @see com.acme.center.platform.iam.domain.model.aggregates.User
+ */
+public record SignUpCommand(String username, String password, List<Role> roles) {
+}
