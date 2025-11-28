89074187040baa6540a6612c6934932d34139eb2
diegovilcasaboya <diegovilcatut@gmail.com>
2025-11-28 11:43:11 -0500

    feat(reports): update report command from resource assembler

--- src/main/java/com/floweytech/agrotrack/platform/reports/interfaces/rest/transform/CreateReportCommandFromResourceAssembler.java
+++ src/main/java/com/floweytech/agrotrack/platform/reports/interfaces/rest/transform/CreateReportCommandFromResourceAssembler.java
@@ -2,6 +2,7 @@ package com.floweytech.agrotrack.platform.reports.interfaces.rest.transform;
 
 import com.floweytech.agrotrack.platform.organization.domain.model.valueobject.OrganizationId;
 import com.floweytech.agrotrack.platform.organization.domain.model.valueobject.PlotId;
+import com.floweytech.agrotrack.platform.profile.domain.model.valueobjects.ProfileId;
 import com.floweytech.agrotrack.platform.reports.domain.model.commands.CreateReportCommand;
 import com.floweytech.agrotrack.platform.reports.domain.model.valueobjects.ReportStatus;
 import com.floweytech.agrotrack.platform.reports.domain.model.valueobjects.ReportType;
@@ -12,11 +13,16 @@ import com.floweytech.agrotrack.platform.reports.interfaces.rest.resources.Creat
  */
 public class CreateReportCommandFromResourceAssembler {
 
-    public static CreateReportCommand toCommandFromResource( CreateReportResource resource ) {
+    public static CreateReportCommand toCommandFromResource(
+            CreateReportResource resource,
+            Long organizationId,
+            Long plotId,
+            Long profileId) {
         return new CreateReportCommand(
-                ReportStatus.valueOf(resource.status()),
-                 new PlotId(resource.plotId()),
-                 new OrganizationId(resource.organizationId()),
+                 new ProfileId(profileId),
+                 ReportStatus.valueOf(resource.status()),
+                 new PlotId(plotId),
+                 new OrganizationId(organizationId),
                ReportType.valueOf(resource.type()),
                 resource.periodStart(),
                 resource.periodEnd()
