f698ebaf34a00150bc305b9bac37ee655d8dc758
diegovilcasaboya <diegovilcatut@gmail.com>
2025-11-28 13:26:09 -0500

    feat(reports): update repository

--- src/main/java/com/floweytech/agrotrack/platform/reports/infrastructure/persistence/jpa/ReportRepository.java
+++ src/main/java/com/floweytech/agrotrack/platform/reports/infrastructure/persistence/jpa/ReportRepository.java
@@ -1,6 +1,9 @@
 package com.floweytech.agrotrack.platform.reports.infrastructure.persistence.jpa;
 
+import com.floweytech.agrotrack.platform.organization.domain.model.valueobject.PlotId;
 import com.floweytech.agrotrack.platform.reports.domain.model.aggregates.Report;
+import com.floweytech.agrotrack.platform.reports.domain.model.valueobjects.ReportPeriod;
+import com.floweytech.agrotrack.platform.reports.domain.model.valueobjects.ReportType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -15,5 +18,7 @@ import java.util.Optional;
  */
 @Repository
 public interface ReportRepository extends JpaRepository<Report,Long> {
+
+    boolean existsByPlotIdAndTypeAndReportPeriod(PlotId plotId, ReportType type, ReportPeriod reportPeriod);
 
 }
