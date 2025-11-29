71645a9550c1b09b64afa0532a168631944223cc
njlmrvllr <anjaliamarovillar@outlook.com>
2025-11-29 18:19:06 -0500

    feat: add server configuration to OpenAPI setup

--- src/main/java/com/floweytech/agrotrack/platform/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
+++ src/main/java/com/floweytech/agrotrack/platform/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
@@ -7,9 +7,12 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenApiConfiguration {
@@ -50,7 +53,9 @@ public class OpenApiConfiguration {
                                         .name(securitySchemeName)
                                         .type(SecurityScheme.Type.HTTP)
                                         .scheme("bearer")
-                                        .bearerFormat("JWT")));
+                                        .bearerFormat("JWT")))
+                .servers(List.of(new Server().url("https://agrotrack-web-service.up.railway.app"))
+                );
 
         return openApi;
     }
