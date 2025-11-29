685f112edddcfdb4b42ee06d64e5627789a49d64
diegovilcasaboya <diegovilcatut@gmail.com>
2025-11-28 19:28:46 -0500

    docs(reports): add report status documentation

--- src/main/java/com/floweytech/agrotrack/platform/reports/domain/model/valueobjects/ReportStatus.java
+++ src/main/java/com/floweytech/agrotrack/platform/reports/domain/model/valueobjects/ReportStatus.java
@@ -1,5 +1,10 @@
 package com.floweytech.agrotrack.platform.reports.domain.model.valueobjects;
 
+/**
+ * ReportStatus enumeration
+ * @summary
+ *
+ */
 public enum ReportStatus {
     CREATED,
     FAILING
